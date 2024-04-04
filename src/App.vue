182fa76600ba6b6f6e54446720a788fc960cb56b
Khloe Leclair <xivkhloeleclair@gmail.com>
2024-04-04 18:32:06 -0400

    Fix things with building.

--- src/App.vue
+++ src/App.vue
@@ -34,10 +34,31 @@
 								{{ error }}
 							</div>
 						</div>
-						<Welcome
-							v-else-if="! original"
-							@pick-files="addFiles"
-						/>
+						<div v-else-if="! original">
+							<div class="mb-4">
+								<h1 class="text-2xl font-semibold">
+									Better Crafting
+								</h1>
+								<h2 class="text-xl font-thin">
+									Custom Crafting Station Converter
+								</h2>
+							</div>
+							<p class="mb-2 text-justify">
+								Hello! This is a quick utility to convert content packs for the
+								<a class="text-pink-600 hover:underline focus:underline" href="https://www.nexusmods.com/stardewvalley/mods/6293" rel="noopener noreferrer">Custom Crafting Stations</a>
+								mod to a format usable by Better Crafting.
+							</p>
+							<p class="text-justify">
+								To use this, just drag the <code class="bg-gray-100 p-1">content.json</code>
+								from a Custom Crafting Stations content pack here, and I'll do the rest.
+							</p>
+							<div class="mt-10 flex justify-center">
+								<button @click="pickFile" class="bg-pink-300 hover:bg-pink-600 focus:bg-pink-600 hover:text-white focus:text-white p-2 rounded-full px-4">
+									<i class="mx-1 fa fa-upload" />
+									Open File
+								</button>
+							</div>
+						</div>
 						<div v-else-if="converted">
 							<div class="mb-4">
 								<h1 class="text-2xl font-semibold">
@@ -78,12 +99,32 @@
 
 <script lang="ts">
 
-import Welcome from './components/Welcome.vue';
 import DropZone from './components/DropZone.vue';
 
 import JSON5 from 'json5';
 
 import { CCSSchema, CPSchema, convert } from './formats.ts';
+
+function openFile(contentType: string, multiple: boolean) {
+	return new Promise<File[]>(resolve => {
+		const input = document.createElement('input') as HTMLInputElement;
+		input.type = 'file';
+		input.accept = contentType;
+		input.multiple = multiple;
+
+		let resolved = false;
+
+		input.onchange = () => {
+			if ( ! resolved ) {
+				resolved = true;
+				const files = Array.from(input.files as unknown as File[]);
+				resolve( files );
+			}
+		}
+
+		input.click();
+	})
+}
 
 function readFile(file: File) {
 
@@ -106,7 +147,6 @@ function readFile(file: File) {
 export default {
 
 	components: {
-		Welcome,
 		DropZone
 	},
 
@@ -130,6 +170,11 @@ export default {
 	},
 
 	methods: {
+		async pickFile() {
+			const files = await openFile('application/json', false);
+			this.addFiles(files);
+		},
+
 		focusMe(event: MouseEvent) {
 			const target = event.target as HTMLTextAreaElement;
 			target.focus();
